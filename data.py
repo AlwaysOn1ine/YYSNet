ba6f0ef252879c6450427c2930d1f8c962bde921
AlwaysOnline <alwaysonline@AlwaysOnlines-MacBook-Pro.local>
2019-04-03 10:48:10 +0800

    第一版可以运行的模型

--- data.py
+++ data.py
@@ -5,66 +5,12 @@ IMAGE_WIDTH = 28
 IMAGE_HEIGHT = 28
 IMAGE_CHANNELS = 3
 
-def read_image(train_path, label_path, batch_size):
-    imagePaths = []
-    labels = []
-    label = 0
+def read_image(train_path, label):
+    image_path = tf.read_file(train_path)
 
-    imgPaths = os.walk('train_path').__next__()[3]
-    for imgpath in imgPaths:
-        if imgpath.endswith('.jpg') or imgpath.endswith('.png'):
-            imagePaths.append(imgpath)
-            labels.append(label)
-            label = label + 1
+    image_decoded = tf.image.decode_png(image_path)
 
-
-    imagePaths = tf.convert_to_tensor(imagePaths, tf.string)
-    labels = tf.convert_to_tensor(labels, tf.int32)
-
-    imagePath, label = tf.train.slice_input_producer([imagePaths, labels], shuffle=True)
-
-    image = tf.read_file(imagePath)
-    image = tf.image.decode_png(image, channels=IMAGE_CHANNELS)
-
-    image = tf.image.resize_images(image, size=[IMAGE_WIDTH, IMAGE_HEIGHT])
-    image = image * 1.0 / 127.5 - 1
-
-    X, Y = tf.train.batch([image, label], batch_size=batch_size, num_threads=4, capacity=batch_size * 8)
-
-    return X, Y
-    # labels = tf.one_hot()
+    image_resized = tf.image.resize_images(image_decoded, [28, 28])
+    return image_resized, label
     
-
-if __name__ == "__main__":
-    imageLists = []
-    labels = []
-    for i in range(20):
-        imageLists.append(i)
-        labels.append(19 - i)
-    
-    imageLists = tf.convert_to_tensor(imageLists, tf.int32)
-    labels = tf.convert_to_tensor(labels, tf.int32)
-
-    imageList, label = tf.train.slice_input_producer([imageLists, labels], shuffle=True)
-
-    X, Y = tf.train.batch([imageList, label], batch_size=2, num_threads=4, capacity=16)
-
-
-    try:
-        with tf.Session() as sess:
-                sess.run(tf.global_variables_initializer())
-                sess.run(tf.local_variables_initializer())
-                coor = tf.train.Coordinator()
-                tf.train.start_queue_runners(sess, coor)
-                while not coor.should_stop():
-                        i, l = sess.run([X, Y])
-                        print(i)
-                        print(l)
-                        print("batch info")
-    except TypeError:
-            print("done")
-    finally:
-            coor.request_stop()
-
-
 
--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import numpy as np
+import os
 from data import read_image
 
-class_count = 100
+CLASS_COUNT = 293
+EPOCH = 1
 
 def weight_variable(shape):
     init = tf.truncated_normal(shape, stddev=0.1)
@@ -19,21 +21,44 @@ def max_pooling2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 if __name__ == "__main__":
-    batch = [1, 1]
+    root, dirs, files = os.walk('./dataset/train_data/').__next__()
+    files = [root + file for file in files]
+    
+    labels = np.arange(0, len(files))
 
-    x = tf.placeholder("float", shape=[None, 784], name="input")
-    y_ = 1
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
+    labels = tf.one_hot(labels, CLASS_COUNT)
+
+    dataset = tf.data.Dataset.from_tensor_slices((files, labels))
+
+    dataset = dataset.map(read_image).shuffle(80)
+
+    dataset1 = dataset.batch(batch_size=CLASS_COUNT)
+
+    dataset = dataset.batch(batch_size=20)
+
+    dataset = dataset.repeat()
+
+    data_it = dataset.make_one_shot_iterator()
+
+    next_data = data_it.get_next()
+
+    # with tf.Session() as sess:
+    #     for i in range(1000):
+    #         image, label = sess.run(next_data)
+    #         print(len(image), len(label), image.shape, np.min(image), np.min(label))
+
+    x = tf.placeholder("float", shape=[None, 28, 28, 3], name="input")
+    y_ = tf.placeholder("float", shape=[None, CLASS_COUNT])
 
     # first conv layer
-    W_conv1 = weight_variable([3, 3, 3, 32])
+    W_conv1 = weight_variable([5, 5, 3, 32])
     b_conv1 = bias_variable([32])
     
-    h_conv1 = tf.nn.relu(conv_2d(x_image, W_conv1) + b_conv1)
+    h_conv1 = tf.nn.relu(conv_2d(x, W_conv1) + b_conv1)
     h_pool1 = max_pooling2x2(h_conv1)
 
     # second conv layer
-    W_conv2 = weight_variable([3, 3, 32, 64])
+    W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
 
     h_conv2 = tf.nn.relu(conv_2d(h_pool1, W_conv2) + b_conv2)
@@ -51,23 +76,30 @@ if __name__ == "__main__":
     h_fc1_dropout = tf.nn.dropout(h_fc1, keep_prob)
 
     # softmax layer
-    W_fc2 = weight_variable([1024, class_count])
-    b_fc2 = bias_variable([class_count])
+    W_fc2 = weight_variable([1024, CLASS_COUNT])
+    b_fc2 = bias_variable([CLASS_COUNT])
 
     y_conv = tf.nn.softmax(tf.matmul(h_fc1_dropout, W_fc2) + b_fc2)
 
     cross_entropy = - tf.reduce_sum(y_ * tf.log(y_conv))
     train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 2))
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_sum(tf.cast(correct_prediction, "float"))
+
+    saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for i in range(2000):
-            print("batch %d trained" % i)
+        for i in range(EPOCH):
+            print(i)
+            batch = sess.run(next_data)
             sess.run(train_step, feed_dict={x : batch[0], y_ : batch[1], keep_prob : 0.5})
-        print(sess.run(accuracy, feed_dict={x : batch[0], y_ : batch[1], keep_prob : 1.0}))
-
+        saver.save(sess, "./model/yysnet%d" % EPOCH)
+        data_1 = dataset1.make_one_shot_iterator()
+        next_data_1 = data_1.get_next()
+        X_train, Y_train = sess.run(next_data_1)
+        print(len(X_train), len(Y_train), X_train.shape)
+        print("Testing Accuracy:", sess.run(accuracy, feed_dict={x : X_train, y_ : Y_train, keep_prob : 1}))
         
 
 
